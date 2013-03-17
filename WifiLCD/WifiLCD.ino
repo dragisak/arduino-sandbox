32525b4ae24850d5055c3195af1a4f17c0c78f76
Dragisa Krsmanovic <dragisak@ticketfly.com>
2013-03-16 23:52:18 -0700

    WiFi + LCD

--- WifiLCD/WifiLCD.ino
+++ WifiLCD/WifiLCD.ino
@@ -1,10 +1,19 @@
-#include "Wifly.h"
+#include <Wifly.h>
 #include <SoftwareSerial.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
 WiflyClass Wifly(2,3);  
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x20 for a 16 chars and 2 line display
+
 void setup()
 {
+  lcd.init();                      // initialize the lcd 
+
   Serial.begin(9600);//use the hardware serial to communicate with the PC
   Serial.print("Start");
+  lcd.backlight();
   Wifly.init();//Initialize the wifishield
   Wifly.setConfig("AsymetricAnimals","laurie1dragisa");//here to set the ssid and password of the Router
   Wifly.join("AsymetricAnimals");
@@ -17,7 +26,8 @@ void loop()
 {
   if(Wifly.canReadFromSocket())
   {
-    Serial.print(Wifly.readFromSocket());
+    //Serial.print(Wifly.readFromSocket());
+    lcd.print(Wifly.readFromSocket());
   }
   if(Serial.available())
   {
